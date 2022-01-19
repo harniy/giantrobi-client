3c1f6243a9643bcbf9f7f4007d484b2942650be3
Kirill Morenkov <kirill.morenkov@devit.group>
2022-01-19 11:38:21 +0200

    env

--- src/api/authApi.js
+++ src/api/authApi.js
@@ -5,6 +5,8 @@ export default function AuthApi() {
 
     const serverUrl = process.env.REACT_APP_SERVER
 
+    console.log(serverUrl)
+
     const userLogin = async (user) => {
         const data = await fetch(`${serverUrl}/login/?username=${user}`)
         return data
